d2db944471756c4ee053db9961f2659859589ffe
BraulioGuevara <guevarabraulio8@gmail.com>
2024-05-26 11:41:28 -0600

    conexion mysql

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 database = mysql.connector.connect(
-    host='localhost',
+    host='pokedex.c4rkvijebh6q.us-east-1.rds.amazonaws.com',
     user='admin',
     password='123698745',
     database='pokedex'
