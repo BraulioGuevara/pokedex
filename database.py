c6de20048b47536043a11ff7d1c985d99aa271a8
oswaldo <oswal.ralo03@gmail.com>
2024-05-28 22:56:06 -0600

    a

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 database = mysql.connector.connect(
-    host='pokedex.csibao6zjk83.us-east-1.rds.amazonaws.com',
+    host='pokedex.czaawgwka6gl.us-east-1.rds.amazonaws.com',
     user='admin',
     password='12345678',
     database='pokedex'
